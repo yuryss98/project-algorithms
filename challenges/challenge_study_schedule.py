9efcc8b01b46303dc1d95075d5502e035396217f
Yury Stolarz <yurysscareer@gmail.com>
2023-04-25 17:35:37 -0300

    feat: add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time and target_time != 0:
+        return None
+
+    count = 0
+    for i in permanence_period:
+        if type(i[0]) != int or type(i[1]) != int:
+            return None
+        if target_time >= i[0] and target_time <= i[1]:
+            count += 1
+
+    return count
