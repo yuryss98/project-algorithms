d7aaa41c51dffad35116f22ff148f38d765618a6
Yury Stolarz <yurysscareer@gmail.com>
2023-04-25 19:49:30 -0300

    feat: add req 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    high_index = len(word) - 1
+    for letter in word:
+        if letter != word[high_index]:
+            return False
+
+        high_index -= 1
+
+    return True
