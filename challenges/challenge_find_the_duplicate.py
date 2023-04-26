f3f265ecc1daacbcfd2dba4431538bda8319e4b0
Yury Stolarz <yurysscareer@gmail.com>
2023-04-26 14:40:06 -0300

    feat: add req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) < 2:
+        return False
+
+    nums_set = set()
+    for number in nums:
+        if not isinstance(number, int) or number < 0:
+            return False
+        if number in nums_set:
+            return number
+        nums_set.add(number)
+
+    return False
